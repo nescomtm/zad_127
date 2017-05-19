e2a401c515949deb13ee988e57b1f4552948ae23
DELL-M4800\TM <tomczyk@nescom.co>
2017-05-19 17:45:07 +0200

    zad 12_7 ver 1 robocza

---
+++ js/App.js
@@ -0,0 +1,49 @@
+// OGÓLNA FUNKCJA
+
+function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '1837',
+    'X-Auth-Token': 'dba9c260b9c3451ae78df194e55f53ff'
+
+ // 'X-Client-Id': 'X-Client-Id',
+ // 'X-Auth-Token': 'X-Auth-Token'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
+
+
+
